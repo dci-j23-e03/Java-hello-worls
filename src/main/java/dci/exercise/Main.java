81ccff781528849c77984e9eafff22402aa8cd9c
dzenang <dzenan.granulo@gmail.com>
2024-01-11 13:47:28 +0100

    Add solution for Task 1

---
+++ src/main/java/dci/exercise/Main.java
@@ -0,0 +1,19 @@
+package dci.exercise;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("Integer value: ");
+        System.out.println(30);
+        System.out.print("Long value: ");
+        System.out.println(50L);
+        System.out.print("Float value: ");
+        System.out.println(4.0f);
+        System.out.print("Double value: ");
+        System.out.println(10.99);
+        System.out.print("Boolean value: ");
+        System.out.println(true);
+        System.out.print("String value: ");
+        System.out.println("so literally what ever text you put here will be printed");
+    }
+}
